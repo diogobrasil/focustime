7a40abf5229ab14d6dece8da8596fea08f59b96e
diogo <diogobrasildasilva@gmail.com>
2024-06-28 02:11:10 -0300

    Implementação da função countdown

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,8 +1,9 @@
 import state from './state.js';
+import * as timer from './timer.js';
 
 export function toggleRunning () {
   state.isRunning = document.documentElement.classList.toggle('running');
-  
+  timer.countdown();
 }
 
 export function set () {
@@ -12,6 +13,7 @@ export function set () {
 export function reset () {
   state.isRunning = false;
   document.documentElement.classList.remove('running');
+  timer.updateDisplay();
 }
 
 export function toggleMusic () {
