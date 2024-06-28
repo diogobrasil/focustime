66815640b2214f1a8597cfbc1349e6eb4eba0f1e
diogo <diogobrasildasilva@gmail.com>
2024-06-28 01:20:25 -0300

    Mudando o estado da aplicação conforme a ação

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,3 +1,19 @@
+import state from './state.js';
+
 export function toggleRunning () {
-  console.log("Toggling running");
+  state.isRunning = document.documentElement.classList.toggle('running');
+  
+}
+
+export function set () {
+
+}
+ 
+export function reset () {
+  state.isRunning = false;
+  document.documentElement.classList.remove('running');
+}
+
+export function toggleMusic () {
+  state.isMute = document.documentElement.classList.toggle('music-on');
 }
